849d788fcadfa546a16c11001cd9fd4a13821bb8
alexrmr08 <walter.romero123.2@gmail.com>
2026-07-11 15:22:00 -0500

    feat: Implementar la clase hija ClaroFactory.java

---
+++ src/ClaroFactory.java
@@ -0,0 +1,22 @@
+public class ClaroFactory extends FabricaCreadora {
+    
+    @Override
+    public PlanDatos crearPlanDatos(String tipo) {
+
+        if (tipo.equalsIgnoreCase("Plan Academico")) {
+            return new PlanDatosAcademico("Claro");
+        
+        }
+
+        if (tipo.equalsIgnoreCase("Plan Persona")) {
+            return new PlanDatosPersona("Claro");
+        }
+
+        if (tipo.equalsIgnoreCase("Plan Negocio")) {
+            return new PlanDatosNegocio("Claro");
+        }
+
+        return null;
+    }
+    
+}
