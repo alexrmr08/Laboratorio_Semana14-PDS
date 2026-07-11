46394caa43339b5d32beb1df6cf2a2c38a629d9e
alexrmr08 <walter.romero123.2@gmail.com>
2026-07-11 15:20:04 -0500

    feat: Implementar la clase hija MovistarFactory.java

---
+++ src/MovistarFactory.java
@@ -0,0 +1,22 @@
+public class MovistarFactory extends FabricaCreadora{
+ 
+    @Override
+    public PlanDatos crearPlanDatos(String tipo) {
+        if (tipo.equalsIgnoreCase("Plan Academico")) {
+            return new PlanDatosAcademico("Movistar");
+        
+        }
+
+        if (tipo.equalsIgnoreCase("Plan Persona")) {
+            return new PlanDatosPersona("Movistar");
+        }
+
+        if (tipo.equalsIgnoreCase("Plan Negocio")) {
+            return new PlanDatosNegocio("Movistar");
+        }
+
+        return null;
+        
+    }
+
+}
